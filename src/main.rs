91c34cf306cfaad61d34201217a87939203d4144
Eduardo Oliveira Alves <eduardo.alves@icc.org.br>
2022-12-05 12:28:02 -0300

    done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
+#![allow(unused_variables, dead_code)]
+
+use std::env;
+use std::process;
+
+mod minigrep;
+use crate::minigrep::minigrep::{Config, run};
 
 fn main() {
-    println!("Hello World!")
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args)
+    .unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments!");
+        process::exit(1)
+    });
+
+    if let Err(err) = run(config) {
+        eprintln!("Application Error!");
+        process::exit(1)
+    }
+
 }
